a6f185c81d248c62951844337ee2542fbc0ce2cb
Vladimir Sinitin <vladimir_sinitin@MacBook-Air-Calin.local>
2023-09-05 16:15:39 +0300

    Blade Directives

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,15 @@
-<div>Hello, I`m a blade template!</div> 
-@isset($name)
-    <div>The name is: {{$name}}</div>
-@endisset
+<h1>The list of tasks</h1>
+
+<div>
+    <!-- @if(count($tasks)) -->
+    @forelse ($tasks as $task)
+    <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}"> {{ $task->title }}</a> 
+    </div>
+    @empty    
+    <div>There are no tasks!</div>
+    @endforelse
+    <!-- @else
+    <div>There are no tasks!</div> -->
+    <!-- @endif -->
+</div>
