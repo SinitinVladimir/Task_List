a635830dc13b66a950e9e79c39f9130b22938034
Vladimir Sinitin <vladimir_sinitin@MacBook-Air-Calin.local>
2023-09-06 19:56:03 +0300

    Forms and CSRF Protection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Models\Task;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,59 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// class Task
-// {
-//     public function __construct(
-//         public int $id,
-//         public string $title,
-//         public string $description,
-//         public ?string $long_description,
-//         public bool $completed,
-//         public string $created_at,
-//         public string $updated_at
-//     ) {
-//     }
-// }
-
-// $tasks = [
-//     new Task(
-//         1,
-//         'Buy groceries',
-//         'Task 1 description',
-//         'Task 1 long description',
-//         false,
-//         '2023-03-01 12:00:00',
-//         '2023-03-01 12:00:00'
-//     ),
-//     new Task(
-//         2,
-//         'Sell old stuff',
-//         'Task 2 description',
-//         null,
-//         false,
-//         '2023-03-02 12:00:00',
-//         '2023-03-02 12:00:00'
-//     ),
-//     new Task(
-//         3,
-//         'Learn programming',
-//         'Task 3 description',
-//         'Task 3 long description',
-//         true,
-//         '2023-03-03 12:00:00',
-//         '2023-03-03 12:00:00'
-//     ),
-//     new Task(
-//         4,
-//         'Take dogs for a walk',
-//         'Task 4 description',
-//         null,
-//         false,
-//         '2023-03-04 12:00:00',
-//         '2023-03-04 12:00:00'
-//     ),
-// ];
-
 Route::get('/', function () {
     return redirect()->route('task.index');
 });
@@ -74,10 +22,11 @@ Route::get('/', function () {
 Route::get('/tasks', function () {
     return view('index', [
         'tasks' => \App\Models\Task::latest()->get() 
-        //->where('completed', true) to show just completed ones
-        //all() -- previous version for all tasks
     ]);
 })->name ('task.index');
+
+Route::view('tasks/create', 'create')
+->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id)  {
     return view('show', [
@@ -85,37 +34,26 @@ Route::get('/tasks/{id}', function ($id)  {
     ]);
 })->name('tasks.show');
 
-// Route::get('/tasks/{id}', function ($id) use ($tasks) {
-//     $task = collect($tasks)->firstWhere('id', $id);
-
-//     if (!$task){
-//         abort(Response::HTTP_NOT_FOUND);
-//     }
-
-//     return view('show', ['task' => $task]);
-// })->name('tasks.show');
 
 Route::fallback(function () {
     return 'Still got somewhere';
 });
 
-// Route::get('/hello', function (){
-//     return 'Hello!';
-// })->name('helllo');
+Route::post('/tasks', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
 
-// Route::get('hallo', function () {
-//     return redirect()->route('helllo');
-// });
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
 
+    $task->save();
 
-// Route::get('/greet/{name}', function ($name) {
-//     return 'Hello ' . $name . '!';
-// });
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 
-
-// Different HTTP methods
-// HTTP verbs
-// Get to read data, fetching 
-// Post to store new data to create
-// Put to modify something
-// Delete to destroy data
+    // dd($request->all());
+})->name('tasks.store');
