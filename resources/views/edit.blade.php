5948f874d46f6718ddbf662beb522281a8286131
Vladimir Sinitin <vladimir_sinitin@MacBook-Air-Calin.local>
2023-09-07 13:54:27 +0300

    Edit Form - Keeping Old Values in the Form - Reusability (Route Model Binding Form Requests, Mass Assignment)

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,7 +2,7 @@
 
 @extends('layouts.app')
 
-@section('title', 'Add Task')
+@section('title', 'Edit Task')
 
 @section('styles')
     <style>
@@ -15,13 +15,14 @@
 
 @section('content')
 
-    <form method="POST" action="{{ route('tasks.store') }}">
+    <form method="POST" action="{{ route('tasks.update', ['task' => $task->id]) }}">
         @csrf
+        @method('PUT')
         <div>
             <label for="title">
                 Title
             </label>
-            <input type="text" name="title" id="title" value="{{ old('title') }}">
+            <input type="text" name="title" id="title" value="{{ $task->title }}">
 
             @error('title')
                 <p class="error-message" >{{ $message }}</p>
@@ -31,7 +32,7 @@
 
 <div>
     <label for="description">Description</label>
-    <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
+    <textarea name="description" id="description" rows="5"> {{$task->description}} </textarea>
 
             @error('description')
                 <p class="error-message" >{{ $message }}</p>
@@ -41,7 +42,7 @@
 
 <div>
     <label for="long_description">Description</label>
-    <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
+    <textarea name="long_description" id="long_description" rows="10"> {{$task->long_description}} </textarea>
 
             @error('long_description')
                 <p class="error-message" >{{ $message }}</p>
@@ -49,7 +50,7 @@
 
 </div>
 
-<button type="submit">Add Task</button>
+<button type="submit">Edit Task</button>
 
     </form>
 
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
@@ -21,16 +22,22 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->get() 
+        'tasks' => Task::latest()->get() 
     ]);
 })->name ('task.index');
 
 Route::view('tasks/create', 'create')
 ->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id)  {
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task
+    ]);
+})->name ('tasks.edit');
+
+Route::get('/tasks/{task}', function (Task $task)  {
     return view('show', [
-        'task' => \App\Models\Task::findOrFail($id)
+        'task' => $task
     ]);
 })->name('tasks.show');
 
@@ -39,21 +46,29 @@ Route::fallback(function () {
     return 'Still got somewhere';
 });
 
-Route::post('/tasks', function (Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
+Route::post('/tasks', function (TaskRequest $request) {
+    // $data = $request->validated();
+    // $task = new Task;
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
 
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
+    $task = Task::create($request->validated());
 
-    $task->save();
-
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
-    // dd($request->all());
 })->name('tasks.store');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    // $data = $request->validated();
+    // $task->title = $data['title'];
+    // $task->description = $data['description'];
+    // $task->long_description = $data['long_description'];
+    // $task->save();
+
+    $task->update($request->validated());
+    
+    return redirect()->route('tasks.show', ['task' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
